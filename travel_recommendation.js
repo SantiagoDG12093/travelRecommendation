c3f30bcec2811b3f4162b5d32542ea5d41c64536
SantiagoDG12093 <santixduque12093@gmail.com>
2025-01-09 23:33:23 -0500

    Avance en el fetch

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,25 +1,31 @@
-// function showRecommendation(event) {
-//     event.preventDefault();
-//     const place = document.getElementById('recommendations').value;
-    
-//     fetch(travel_recommendation_api.json)
-//         .then(response => response.json())
-//         .then(data => {
-//             const australiaCities = data.countries.find(country => country.name === "Australia").cities;
-//             console.log('Ciudades en Australia:', australiaCities);
-//         //   const infoRecommendation = document.getElementById('infoRecommendation');
-//         //   infoRecommendation.innerHTML = `<h2>Weather in ${data.name}</h2>
-//         //                           <p>Temperature: ${data.main.temp} &#8451;</p>
-//         //                           <p>Weather: ${data.weather[0].description}</p>`;
-//         })
-//         .catch(error => {
-//           console.error('Error fetching recommendation:', error);
-//           const infoRecommendation = document.getElementById('infoRecommendation');
-//           infoRecommendation.innerHTML = `<p>Failed to fetch recommendation. Please try again.</p>`;
-//         });
-// }
+function showRecommendation(event) {
+    event.preventDefault();
+    const place = document.getElementById('recommendations').value.toLowerCase();
 
-function showRecommendation(){
-    console.log('click');
+    fetch('../travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            var recommendation;
+            if (place == 'country' || place == 'countries') {
+                recommendation = data.countries;
+            }
+            else if (place == 'temple' || place == 'temples') {
+                recommendation = data.temples;
+            }
+            else if (place == 'beach' || place == 'beaches') {
+                recommendation = data.beaches;
+            }
+            // const recommendation = data.countries.find(country => country.name === "Australia").cities;
+        //   const infoRecommendation = document.getElementById('infoRecommendation');
+        //   infoRecommendation.innerHTML = `<h2>Weather in ${data.name}</h2>
+        //                           <p>Temperature: ${data.main.temp} &#8451;</p>
+        //                           <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+          console.error('Error fetching recommendation:', error);
+          const infoRecommendation = document.getElementById('infoRecommendation');
+          infoRecommendation.innerHTML = `<p>Failed to fetch recommendation. Please try again.</p>`;
+        });
 }
+
 document.getElementById('btnSearch').addEventListener('click',showRecommendation );
