a6a96b5ac0cb4601f25f600e2516527fc0734f39
SantiagoDG12093 <santixduque12093@gmail.com>
2025-01-15 19:46:05 -0500

    Avance en mostrar recomendaciones

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,20 +1,24 @@
 function showRecommendation(event) {
     event.preventDefault();
     const place = document.getElementById('recommendations').value.toLowerCase();
-
+    
     fetch('../travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            var recommendation;
+            var recommendation_1;
+            var recommendation_2;
+
             if (place == 'country' || place == 'countries') {
-                recommendation = data.countries;
+                const [random_1, random_2] = generateRandom();
+                recommendation_1 = data.countries[random_1].cities[random_2];
             }
             else if (place == 'temple' || place == 'temples') {
-                recommendation = data.temples;
+                recommendation_1 = data.temples;
             }
             else if (place == 'beach' || place == 'beaches') {
-                recommendation = data.beaches;
+                recommendation_1 = data.beaches;
             }
+            console.log(recommendation_1);
             // const recommendation = data.countries.find(country => country.name === "Australia").cities;
         //   const infoRecommendation = document.getElementById('infoRecommendation');
         //   infoRecommendation.innerHTML = `<h2>Weather in ${data.name}</h2>
@@ -26,6 +30,12 @@ function showRecommendation(event) {
           const infoRecommendation = document.getElementById('infoRecommendation');
           infoRecommendation.innerHTML = `<p>Failed to fetch recommendation. Please try again.</p>`;
         });
+}
+
+function generateRandom() {
+    const random_1 = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    const random_2 = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+    return [random_1, random_2];
 }
 
 document.getElementById('btnSearch').addEventListener('click',showRecommendation );
