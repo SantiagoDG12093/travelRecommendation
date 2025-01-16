1805b7752daa66adc49e3bf27f915c035fe7dbd9
SantiagoDG12093 <santixduque12093@gmail.com>
2025-01-15 21:42:48 -0500

    Proyecto final completado sin optional task

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,23 +7,42 @@ function showRecommendation(event) {
         .then(data => {
             var recommendation_1;
             var recommendation_2;
+            var [random_1, random_2] = [0, 0];
 
             if (place == 'country' || place == 'countries') {
-                const [random_1, random_2] = generateRandom();
+                [random_1, random_2]= generateRandom();
                 recommendation_1 = data.countries[random_1].cities[random_2];
+                [random_1, random_2]= generateRandom();
+                recommendation_2 = data.countries[random_1].cities[random_2];
+                
+                while (recommendation_1.name === recommendation_2.name) {
+                    [random_1, random_2]= generateRandom();
+                    recommendation_2 = data.countries[random_1].cities[random_2];
+                }
             }
             else if (place == 'temple' || place == 'temples') {
-                recommendation_1 = data.temples;
+                recommendation_1 = data.temples[0];
+                recommendation_2 = data.temples[1];
             }
             else if (place == 'beach' || place == 'beaches') {
-                recommendation_1 = data.beaches;
+                recommendation_1 = data.beaches[0];
+                recommendation_2 = data.beaches[1];
             }
-            console.log(recommendation_1);
-            // const recommendation = data.countries.find(country => country.name === "Australia").cities;
-        //   const infoRecommendation = document.getElementById('infoRecommendation');
-        //   infoRecommendation.innerHTML = `<h2>Weather in ${data.name}</h2>
-        //                           <p>Temperature: ${data.main.temp} &#8451;</p>
-        //                           <p>Weather: ${data.weather[0].description}</p>`;
+            const infoRecommendation = document.getElementById('infoRecommendation');
+            infoRecommendation.innerHTML = `<div id="recommendation">
+                                                <img src="${recommendation_1.imageUrl}" id="imgRecommendation" alt="${recommendation_1.name}">
+                                                <div id="textRecommendation">
+                                                    <h1>${recommendation_1.name}</h1>
+                                                    <p>${recommendation_1.description}</p>
+                                                </div>
+                                            </div>
+                                            <div id="recommendation">
+                                                <img src="${recommendation_2.imageUrl}" id="imgRecommendation" alt="${recommendation_2.name}">
+                                                <div id="textRecommendation">
+                                                    <h1>${recommendation_2.name}</h1>
+                                                    <p>${recommendation_2.description}</p>
+                                                </div>
+                                            </div>`;
         })
         .catch(error => {
           console.error('Error fetching recommendation:', error);
@@ -39,3 +58,7 @@ function generateRandom() {
 }
 
 document.getElementById('btnSearch').addEventListener('click',showRecommendation );
+
+document.getElementById('btnReset').addEventListener('click', function() {
+    document.getElementById('infoRecommendation').innerHTML = ''; 
+});
